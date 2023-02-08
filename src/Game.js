f7973ea640b7442eb17fbd11d63ad949d34f8f68
Anupam Vamsi <anupam.ismav@gmail.com>
2023-02-08 09:56:50 +0530

    1. Remove prevTurn and currTurn variables and loop function since they are unused

--- src/Game.js
+++ src/Game.js
@@ -20,9 +20,6 @@ const Game = () => {
   const p2s3L3 = Ship(3);
   const p2s4L5 = Ship(5);
 
-  let prevTurn = p2;
-  let currTurn = p1;
-
   const manualSetup = () => {
     p1.gameboard.placeShip(p1s1L2, 1, 1, true);
     p1.gameboard.placeShip(p1s2L2, 4, 5);
@@ -33,83 +30,6 @@ const Game = () => {
     p2.gameboard.placeShip(p2s2L2, 6, 3, true);
     p2.gameboard.placeShip(p2s3L3, 0, 3, true);
     p2.gameboard.placeShip(p2s4L5, 2, 6);
-  };
-
-  const loop = () => {
-    // 128 is the total number of squares for both players
-    for (let i = 0; i < 128; i += 1) {
-      if (currTurn === p1) {
-        // miss attack
-        // currTurn.enemy.gameboard.receiveAttack(1, 1);
-
-        // player won
-        // p2.gameboard.receiveAttack(3, 2);
-        // p2.gameboard.receiveAttack(4, 2);
-
-        // p2.gameboard.receiveAttack(6, 3);
-        // p2.gameboard.receiveAttack(6, 4);
-
-        // p2.gameboard.receiveAttack(0, 3);
-        // p2.gameboard.receiveAttack(0, 4);
-        // p2.gameboard.receiveAttack(0, 5);
-
-        // p2.gameboard.receiveAttack(2, 6);
-        // p2.gameboard.receiveAttack(3, 6);
-        // p2.gameboard.receiveAttack(4, 6);
-        // p2.gameboard.receiveAttack(5, 6);
-        // p2.gameboard.receiveAttack(6, 6);
-
-        currTurn = p2;
-
-        // console.log('enemy p1 down?', p1.gameboard.allShipsSunk);
-        // console.log('enemy p2 down?', p2.gameboard.allShipsSunk);
-
-        if (p2.gameboard.allShipsSunk) {
-          break;
-        }
-      }
-
-      // console.log('currTurn is p2', currTurn === p2);
-      if (currTurn === p2) {
-        // miss attack
-        // currTurn.enemy.gameboard.receiveAttack(3, 4);
-
-        // player lost
-        // p1.gameboard.receiveAttack(1, 1);
-        // p1.gameboard.receiveAttack(1, 2);
-
-        // p1.gameboard.receiveAttack(4, 5);
-        // p1.gameboard.receiveAttack(5, 5);
-
-        // p1.gameboard.receiveAttack(4, 1);
-        // p1.gameboard.receiveAttack(5, 1);
-        // p1.gameboard.receiveAttack(6, 1);
-
-        // p1.gameboard.receiveAttack(2, 3);
-        // p1.gameboard.receiveAttack(2, 4);
-        // p1.gameboard.receiveAttack(2, 5);
-        // p1.gameboard.receiveAttack(2, 6);
-        // p1.gameboard.receiveAttack(2, 7);
-
-        currTurn = p1;
-
-        if (p1.gameboard.allShipsSunk) {
-          break;
-        }
-
-        // console.log('enemy p1 down?', p1.gameboard.allShipsSunk);
-        // console.log('enemy p2 down?', p2.gameboard.allShipsSunk);
-      }
-    }
-
-    if (p1.gameboard.allShipsSunk) {
-      return false; // player lost
-    }
-    if (p2.gameboard.allShipsSunk) {
-      return true; // player won
-    }
-
-    return null;
   };
 
   return {
@@ -129,20 +49,7 @@ const Game = () => {
     get p2() {
       return p2;
     },
-    get prevTurn() {
-      return prevTurn;
-    },
-    set prevTurn(player) {
-      prevTurn = player;
-    },
-    get currTurn() {
-      return currTurn;
-    },
-    set currTurn(player) {
-      currTurn = player;
-    },
     manualSetup,
-    loop,
   };
 };
 
--- tests/Game.test.js
+++ tests/Game.test.js
@@ -12,52 +12,5 @@ describe('Game loop', () => {
   test('Setup game', () => {
     expect(game.p1.enemy).toBe(game.p2);
     expect(game.p2.enemy).toBe(game.p1);
-
-    expect(game.prevTurn).toBe(game.p2);
-    expect(game.currTurn).toBe(game.p1);
-  });
-
-  test('Player 1 loses / Player 2 (computer) wins', () => {
-    expect(game.p1.gameboard.receiveAttack(1, 1)).toBe(true);
-    expect(game.p1.gameboard.receiveAttack(1, 2)).toBe(true);
-
-    expect(game.p1.gameboard.receiveAttack(4, 5)).toBe(true);
-    expect(game.p1.gameboard.receiveAttack(5, 5)).toBe(true);
-
-    expect(game.p1.gameboard.receiveAttack(4, 1)).toBe(true);
-    expect(game.p1.gameboard.receiveAttack(5, 1)).toBe(true);
-    expect(game.p1.gameboard.receiveAttack(6, 1)).toBe(true);
-
-    expect(game.p1.gameboard.receiveAttack(2, 3)).toBe(true);
-    expect(game.p1.gameboard.receiveAttack(2, 4)).toBe(true);
-    expect(game.p1.gameboard.receiveAttack(2, 5)).toBe(true);
-    expect(game.p1.gameboard.receiveAttack(2, 6)).toBe(true);
-    expect(game.p1.gameboard.receiveAttack(2, 7)).toBe(true);
-
-    expect(game.loop()).toBe(false);
-  });
-
-  test('Player 1 wins / Player 2 (computer) loses', () => {
-    expect(game.p2.gameboard.receiveAttack(3, 2)).toBe(true);
-    expect(game.p2.gameboard.receiveAttack(4, 2)).toBe(true);
-
-    expect(game.p2.gameboard.receiveAttack(6, 3)).toBe(true);
-    expect(game.p2.gameboard.receiveAttack(6, 4)).toBe(true);
-
-    expect(game.p2.gameboard.receiveAttack(0, 3)).toBe(true);
-    expect(game.p2.gameboard.receiveAttack(0, 4)).toBe(true);
-    expect(game.p2.gameboard.receiveAttack(0, 5)).toBe(true);
-
-    expect(game.p2.gameboard.receiveAttack(2, 6)).toBe(true);
-    expect(game.p2.gameboard.receiveAttack(3, 6)).toBe(true);
-    expect(game.p2.gameboard.receiveAttack(4, 6)).toBe(true);
-    expect(game.p2.gameboard.receiveAttack(5, 6)).toBe(true);
-    expect(game.p2.gameboard.receiveAttack(6, 6)).toBe(true);
-
-    expect(game.loop()).toBe(true);
-  });
-
-  test('No one wins', () => {
-    expect(game.loop()).toBe(null);
   });
 });
